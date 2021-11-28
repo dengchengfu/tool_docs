8895a719febcdc797348fa9b5c5f732e0657bf26
CHENGFU DENG <dengchengfu.123@gmail.com>
2021-11-28 12:53:17 +0800

    Add files via upload

---
+++ pagic.config.tsx
@@ -0,0 +1,58 @@
+// import { React } from 'https://deno.land/x/pagic/mod.ts';
+
+export default {
+  srcDir: '.',
+  exclude: ['LICENSE'],
+  root: '/tool_docs/',
+
+  theme: 'docs',
+  plugins: ['sidebar', 'prev_next'],
+  title: '阿甫工具-说明手册',
+  description: 'Use this template to create a Pagic site with the docs theme',
+  // To use jsx syntax, please rename this file to pagic.config.tsx
+  // head: <>
+  //   <link rel="icon" type="image/png" href="/favicon.png" />
+  //   <script src="/assets/custom.js" />
+  // </>,
+  nav: [
+    {
+      text: '赞助',
+      link: '/tool_docs/introduction/index.html',
+    },
+    {
+      text: '加入微信群',
+      link: 'https://pagic.org/',
+    },
+    {
+      text: 'About',
+      link: '/tool_docs/about/index.html',
+      align: 'right',
+    },
+  ],
+  github: 'https://github.com/dengchengfu/tool_docs',
+  sidebar: {
+    '/': [
+      'introduction/README.md',
+      {
+        link: 'test_pages/README.md',
+        children: ['test_pages/markdown_test.md', 'test_pages/front_matter.md', 'test_pages/react_hooks_test.tsx'],
+      },
+      {
+        text: 'Folder',
+        children: [
+          'folder/foo.md',
+          {
+            text: 'Custom sidebar text',
+            link: 'folder/bar.md'
+          }
+        ]
+      }
+    ],
+  },
+  tools: {
+    editOnGitHub: true,
+    backToTop: true,
+  },
+
+  port: 8000,
+};
