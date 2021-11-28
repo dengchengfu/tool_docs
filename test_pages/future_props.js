a068269b9c63a2485c34238ff0f215bc7257bc99
dengchengfu <dengchengfu@users.noreply.github.com>
2021-11-28 08:40:03 +0000

    deploy: fd181078c8a7b65fda9c556a2687db45e61a8b38

--- test_pages/future_props.js
+++ test_pages/future_props.js
@@ -11,7 +11,7 @@ export default {
     'outputPath': "test_pages/future.html",
     'title': "未来规划",
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<h1>未来规划</h1>\n<p>到了取数的高级层次，GTA目标拆解是数字营销分析中很重要的阶段。但由于各家的拆解方案，指标侧重点各有不同、各有偏好，且遇到大于5000w的人群数量还得切分取数，所以打算最终以一种参数配置的界面呈现出来（初步设想）。</p>\n<p>初期会上线各家都通用的、简单、基础、浅层的取数及目标监控功能，之后逐渐加入各类自定义条件，终极目标是期望实现GTA的全程自动运算出数。</p>'
+            __html: '<h1>未来规划</h1>\n<p>到了取数的高级层次，GTA目标拆解是数字营销分析中很重要的阶段。但由于各家的拆解方案，指标侧重点各有不同、各有偏好，且遇到大于5000w的人群数量还得切分取数，所以打算最终以一种参数配置的界面呈现出来（初步设想）。</p>\n<p>初期会上线各家都通用的、简单、基础、浅层的取数及目标监控功能，之后逐渐加入各类自定义条件，终极目标是实现GTA的全程自动运算出数。</p>'
         } }),
     'head': React.createElement(React.Fragment, null,
         React.createElement("link", { crossOrigin: "anonymous", href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css", integrity: "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X", rel: "stylesheet" })),
@@ -24,7 +24,7 @@ export default {
         } }, "\u00A9 CHENGFU"),
     'contentTitle': React.createElement("h1", { key: "0" }, "\u672A\u6765\u89C4\u5212"),
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<p>到了取数的高级层次，GTA目标拆解是数字营销分析中很重要的阶段。但由于各家的拆解方案，指标侧重点各有不同、各有偏好，且遇到大于5000w的人群数量还得切分取数，所以打算最终以一种参数配置的界面呈现出来（初步设想）。</p>\n<p>初期会上线各家都通用的、简单、基础、浅层的取数及目标监控功能，之后逐渐加入各类自定义条件，终极目标是期望实现GTA的全程自动运算出数。</p>'
+            __html: '<p>到了取数的高级层次，GTA目标拆解是数字营销分析中很重要的阶段。但由于各家的拆解方案，指标侧重点各有不同、各有偏好，且遇到大于5000w的人群数量还得切分取数，所以打算最终以一种参数配置的界面呈现出来（初步设想）。</p>\n<p>初期会上线各家都通用的、简单、基础、浅层的取数及目标监控功能，之后逐渐加入各类自定义条件，终极目标是实现GTA的全程自动运算出数。</p>'
         } }),
     'toc': null,
     'author': "CHENGFU DENG",
@@ -32,7 +32,7 @@ export default {
         "CHENGFU DENG"
     ],
     'date': "2021-11-28T04:11:10.000Z",
-    'updated': "2021-11-28T07:50:35.000Z",
+    'updated': "2021-11-28T08:38:30.000Z",
     'excerpt': "到了取数的高级层次，GTA目标拆解是数字营销分析中很重要的阶段。但由于各家的拆解方案，指标侧重点各有不同、各有偏好，且遇到大于5000w的人群数量还得切分取数，所以打算最终以一种参数配置的界面呈现出来（初步设想）。 初期...",
     'cover': undefined,
     'sidebar': [
